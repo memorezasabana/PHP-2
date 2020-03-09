91895bca2b49189d605d264d8e7daa2a2d4aaae8
Memoreza Sabana <53498578+memorezasabana@users.noreply.github.com>
2020-03-09 15:45:53 +0700

    Update hitung.php

--- hitung.php
+++ hitung.php
@@ -1,19 +1,20 @@
 <?php
+function luas_kerucut($r,$s){
+    return 3.14 * $r *($r+$s);
+}
+function volume_kerucut($r,$t){
+    return 3.14 * $r *$r*$t;
+}
 $jari = $_POST["jari"];
 $sisi = $_POST["sisi"];
 $tinggi = $_POST["tinggi"];
-$phi = 3.14;
-
-
 if ($_POST["submit"] == "LUAS"){
-    $hasil = $phi * $jari *($jari+$sisi);
     echo "<h4>LUAS PERMUKAAN KERUCUT</h4>";
-    echo "<h4>$phi x $jari x ($jari + $sisi) = $hasil<h4>";
+    echo "<h4>3.14 x $jari x ($jari + $sisi) = "; echo luas_kerucut($jari,$sisi);
 }
 else if($_POST["submit"] == "VOLUME"){
-    $hasil = 1/3 * $phi * $jari * $jari  *$tinggi ;
     echo "<h4>VOLUME KERUCUT</h4>";
-    echo "<h4>1/3 x $phi x $jari x $jari x $tinggi = $hasil<h4>";
+    echo "<h4>1/3 x 3.14 x $jari x $jari x $tinggi = "; echo volume_kerucut($jari,$sisi);
 }
 
 ?>
